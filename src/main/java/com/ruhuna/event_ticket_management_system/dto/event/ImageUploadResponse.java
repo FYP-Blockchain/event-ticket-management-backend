2d7cad3e63032fde0b19595bf321dd37fa10f426
Pabasara715 <pabasaravihanga715@gmail.com>
2025-08-19 23:34:37 +0530

    New dtos for EventMetadata, ImageUploadResponse, UpdateEventRequest

---
+++ src/main/java/com/ruhuna/event_ticket_management_system/dto/event/ImageUploadResponse.java
@@ -0,0 +1,12 @@
+package com.ruhuna.event_ticket_management_system.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ImageUploadResponse {
+    private String imageUrl;
+}
---
+++ src/main/java/com/ruhuna/event_ticket_management_system/dto/event/UpdateEventRequest.java
@@ -0,0 +1,44 @@
+package com.ruhuna.event_ticket_management_system.dto.event;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class UpdateEventRequest {
+    // On-chain data
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Event date cannot be blank")
+    private String eventDateUTC;
+
+    @Min(value = 1, message = "Total supply must be at least 1")
+    private Long totalSupply;
+
+    @Min(value = 0, message = "Price in Ether must be at least 0")
+    private BigDecimal priceInEther;
+
+    // Off-chain (IPFS) data
+    @NotBlank(message = "Description cannot be blank")
+    private String description;
+
+    @NotBlank(message = "Event start time cannot be blank")
+    private String eventStartTime;
+
+    @NotBlank(message = "Event end time cannot be blank")
+    private String eventEndTime;
+
+    @NotBlank(message = "Image URL cannot be blank")
+    private String image;
+
+    @NotBlank(message = "Category cannot be blank")
+    private String category;
+
+    @NotBlank(message = "location cannot be blank")
+    private String location;
+}
