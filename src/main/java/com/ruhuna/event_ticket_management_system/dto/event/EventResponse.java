07e47f441c4624185d00a0efd55720855f308cff
Pabasara715 <pabasaravihanga715@gmail.com>
2025-08-19 23:43:11 +0530

    update event response

--- src/main/java/com/ruhuna/event_ticket_management_system/dto/event/EventResponse.java
+++ src/main/java/com/ruhuna/event_ticket_management_system/dto/event/EventResponse.java
@@ -1,5 +1,6 @@
 package com.ruhuna.event_ticket_management_system.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import java.math.BigInteger;
@@ -8,16 +9,23 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Builder
-@RequiredArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EventResponse {
     private String id;
     private String name;
     private String organizerAddress;
     private LocalDateTime eventDate;
     private Long totalSupply;
-    private BigInteger priceInEther;
+    private BigInteger priceInWei;
     private String metadataURI;
-    private String imageUrl;
     private boolean active;
+
+    private String description;
+    private String imageUrl;
+    private String location;
+    private String eventStartTime;
+    private String eventEndTime;
+    private String category;
 }
