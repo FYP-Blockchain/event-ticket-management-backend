b98cc787cfb949f0c8791d36a055a0cb3ce47d42
Pabasara715 <pabasaravihanga715@gmail.com>
2025-08-19 23:42:49 +0530

    update createventRequest

--- src/main/java/com/ruhuna/event_ticket_management_system/dto/event/CreateEventRequest.java
+++ src/main/java/com/ruhuna/event_ticket_management_system/dto/event/CreateEventRequest.java
@@ -3,22 +3,42 @@ package com.ruhuna.event_ticket_management_system.dto.event;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 public class CreateEventRequest {
-    @NotBlank(message = "Event name cannot be blank")
+    // On-chain data
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
     @NotBlank(message = "Event date cannot be blank")
-    private String eventDateUTC; ;
-    @Min(value = 1, message = "Total supply must be at least 1")
+    private String eventDateUTC;
+
+    @Min(value = 1)
     private Long totalSupply;
-    @Min(value = 0, message = "Price in Ether must be at least 0")
+
+    @Min(value = 0)
     private BigDecimal priceInEther;
-    private String metadataURI;
+
+    // Off-chain (IPFS) data
+    @NotBlank(message = "Description cannot be blank")
+    private String description;
+
+    @NotBlank(message = "Event start time cannot be blank")
+    private String eventStartTime;
+
+    @NotBlank(message = "Event end time cannot be blank")
+    private String eventEndTime;
+
+    @NotBlank(message = "Image URL cannot be blank")
+    private String image;
+
+    @NotBlank(message = "Category cannot be blank")
+    private String category;
+
+    @NotBlank(message = "location cannot be blank")
+    private String location;
 }
