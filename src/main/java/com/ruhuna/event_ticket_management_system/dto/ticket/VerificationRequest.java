0722d1f6ab4539090820216be051ffba44bcd41b
Tharindu <tharindugeethanjan@gmail.com>
2025-10-17 09:04:26 +0530

    ticket verifier basic with logical errors #19

---
+++ src/main/java/com/ruhuna/event_ticket_management_system/dto/ticket/VerificationRequest.java
@@ -0,0 +1,24 @@
+package com.ruhuna.event_ticket_management_system.dto.ticket;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class VerificationRequest {
+    @NotBlank(message = "Token Id cannot be blank")
+    private String tokenId;
+
+    @NotBlank(message = "Event Id cannot be blank")
+    private String eventId;
+
+    @NotBlank(message = "Wallet Address cannot be blank")
+    private String walletAddress;
+
+    @NotBlank(message = "Secret Nonce cannot be blank")
+    private String secretNonce;
+
+    @NotBlank(message = "IPFS CID cannot be blank")
+    private String ipfsCid;
+}
